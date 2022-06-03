0286ea2088e115e3dfff5e799be0f744601d0623
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-06-03 10:04:43 +0300

    Step 1 start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from "react";
+
+class App extends Component{
+  state = {
+    contacts: [],
+    name:'',
+  }
+
+  render() {
+    return (
+      <>
+        <h2>PhoneBook</h2>
+            <input
+  type="text"
+  name="name"
+  pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+  title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+  required
+        />
+        <button type="button">Add contact</button>
+        <h2>Contacts</h2>
+      </>
+    )
+  }
+}
+
+export default App;
