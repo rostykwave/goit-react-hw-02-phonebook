f8557b6de55050f35270cd069261a9298526bbdd
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-06-18 22:04:49 +0300

    form class

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,23 @@
 import { Component } from "react";
+import Form from "./Form/Form";
 
 class App extends Component{
   state = {
     contacts: [],
-    name:'',
+   
   }
+
+ 
 
   render() {
     return (
       <>
         <h2>PhoneBook</h2>
-            <input
-  type="text"
-  name="name"
-  pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-  title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-  required
-        />
-        <button type="button">Add contact</button>
+
+<Form/>
+      
         <h2>Contacts</h2>
+        <ul></ul>
       </>
     )
   }
---
+++ src/components/Form/Form.js
@@ -0,0 +1,70 @@
+import { Component } from 'react';
+
+class Form extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  // handleNameChange = e => {
+  //   this.setState({ name: e.currentTarget.value });
+  // }
+
+  // handleNumberChange = e => {
+  //   this.setState({ number: e.currentTarget.value });
+  // }
+
+  handleChange = e => {
+    // console.log(e.currentTarget);
+    // console.log(e.currentTarget.name);
+    // console.log(e.currentTarget.value);
+
+    // this.setState({
+    //   [e.currentTarget.name]:e.currentTarget.value,
+    // });
+
+    const { name, value } = e.currentTarget;
+
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name
+          <input
+            type="text"
+            value={this.state.name}
+            onChange={this.handleChange}
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </label>
+
+        {/* <label>
+            number
+            <input
+              type="tel"
+                   value={this.state.number}
+              onChange={this.handleChange}
+  name="number"
+  pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+  title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+  required
+/>
+          </label> */}
+        <button type="submit">Add contact</button>
+      </form>
+    );
+  }
+}
+
+export default Form;
